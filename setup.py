227fd340967045cfb4c5a987323e55d7a2be6cd5
p0lux <kylian.p@protonmail.ch>
2020-01-17 10:00:58 +0100

    Update requirements & starting setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
 #! /usr/bin/env python3
 from setuptools import setup
 
-
 setup(
-   name='Scraper for League of Legends champions',
+   name='Scraper_championsgg',
    version='1.0',
    description='Module for getting informations for League of Legend champions',
    author='p0',
    author_email='kylian.p@protonmail.ch',
-   packages=['League of Legend Scraper'], 
-   install_requires=['requests', 'beautifulsoup4'],
+   packages=['Scraper_championsgg'], 
+   install_requires=['requests', 'beautifulsoup4', 'termcolor'],
    scripts=[
    			'modules/get_items',
    			'modules/get_runes'
